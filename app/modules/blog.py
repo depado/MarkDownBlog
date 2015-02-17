3c21fa490fcf0ac62541cf27e169664004d7e6f8
Depado <paul.lhussiez@gmail.com>
2015-02-17 16:56:09 +0100

    Rss Feed ?

--- app/modules/blog.py
+++ app/modules/blog.py
@@ -1,13 +1,19 @@
 # -*- coding: utf-8 -*-
 
 from sqlalchemy import desc
-from flask import Blueprint, render_template, url_for, redirect
+from flask import Blueprint, render_template, url_for, redirect, request
 from flask_login import current_user
 from jinja2 import utils
+from urllib.parse import urljoin
+from werkzeug.contrib.atom import AtomFeed
 
 from app.models import User, Post
 
 blueprint = Blueprint('blog', __name__, subdomain="<user_slug>")
+
+
+def make_external(url):
+    return urljoin(request.url_root, url)
 
 
 def requested_blog_user(user_slug):
@@ -38,6 +44,29 @@ def generate_background_css(blog_user=None):
             background_cover = False
     background_css = backgroud_css_template.format(background_url=background_url, repeat_mode=repeat_mode)
     return dict(background_css=background_css, background_cover=background_cover)
+
+
+@blueprint.route("/recent.atom")
+def rss_feed(user_slug):
+    blog_user = requested_blog_user(user_slug)
+    if blog_user:
+        feed = AtomFeed(
+            '{user} Recent Articles'.format(user=blog_user.username),
+            feed_url=request.url,
+            url=request.url_root
+        )
+        posts = blog_user.posts.order_by(desc(Post.pub_date)).limit(15).all()
+        for post in posts:
+            feed.add(post.title, post.content_as_html(),
+                     content_type='html',
+                     author=post.user.username,
+                     url=make_external(url_for("blog.get", user_slug=user_slug, post_slug=post.title_slug)),
+                     updated=post.pub_date,
+                     published=post.pub_date)
+        return feed.get_response()
+    else:
+        return ""
+
 
 
 @blueprint.route("/")
